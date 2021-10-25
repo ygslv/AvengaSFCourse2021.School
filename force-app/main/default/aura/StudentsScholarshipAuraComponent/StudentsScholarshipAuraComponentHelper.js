c2a223cf6984f400ff22cd307cc816fee3ef632a
Yurii Harasym <why.did.i.become.ashen@gmail.com>
2021-10-25 20:01:55 +0300

    Edit aura contoller (hyperlinks)
    +hyperlinks for student names
    -checboxes

--- force-app/main/default/aura/StudentsScholarshipAuraComponent/StudentsScholarshipAuraComponentHelper.js
+++ force-app/main/default/aura/StudentsScholarshipAuraComponent/StudentsScholarshipAuraComponentHelper.js
@@ -2,7 +2,8 @@
     getStudentsHelper : function(component, event, helper) {
 
         component.set('v.columns', [
-            {label:'Student Name', fieldName:'Name', type:'text'},
+            {label:'Student Name', fieldName:'linkName', type:'url', 
+            typeAttributes: {label: { fieldName: 'Name' }, target: '_blank'}},
             {label:'Scholarship', fieldName:'Scholarship__c', type:'text'}
         ]);
 
@@ -12,8 +13,15 @@
             
             if (response.getState() === 'SUCCESS') {
 
-                console.log(response.getReturnValue());
-                component.set('v.stList', response.getReturnValue());
+                var records = response.getReturnValue();
+
+                records.forEach(function(record){
+                    
+                    record.linkName = '/'+record.Id;
+                });
+
+                console.log(records);
+                component.set('v.stList', records);
             }
         })
 
